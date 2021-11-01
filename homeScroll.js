dd1d01b82cd52db95ba0ab4010997e668afa98b3
Syb-O <Usman.s97k@gmail.com>
2021-11-01 08:36:17 +0800

    .

--- homeScroll.js
+++ homeScroll.js
@@ -9,3 +9,27 @@ window.addEventListener('scroll', ()=>{
            text.classList.remove('active');
        }
 });
+
+
+window.addEventListener('load', ()=>{introduction(); introduction2(); add_img()})
+
+function introduction(){
+    let introOne = document.querySelector('.btmRight');
+    setTimeout(() => {
+        introOne.classList.add('readyOne')
+    }, 2000);
+}
+function introduction2(){
+    let introOne = document.querySelector('.topLeft');
+    setTimeout(() => {
+        introOne.classList.add('readyTwo')
+    }, 500);
+}
+
+function add_img() { 
+	let img = document.querySelector('.ourLogo'); 
+    setTimeout(()=> {
+        img.classList.add('ourLogoReady')
+    }, 1500);
+	
+}
